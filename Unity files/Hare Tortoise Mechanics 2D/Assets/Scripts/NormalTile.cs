0479be50df180cd437c9fa90c8cfe8ae32bbee2a
Aaron M <aaron95mulligan@gmail.com>
2018-07-18 01:44:38 +0100

    - Added code to record what tile the player is currently on both as a int and by tile name - Added code to change the colour of the tiles to green on hover over if player can afford it (and red if they cannot) - Added tortoise tiles to the board which are a different colour to stand out (code not included for now) - Added UI to the game to show player currency - Tried adding code to increase the currency cost per move however is not currently working - Added screenshots folder for each commits comments

--- Unity files/Hare Tortoise Mechanics 2D/Assets/Scripts/NormalTile.cs
+++ Unity files/Hare Tortoise Mechanics 2D/Assets/Scripts/NormalTile.cs
@@ -8,6 +8,22 @@ public class NormalTile : MonoBehaviour {
     public float playerSpeed = 0.5f;
     public Player player;
 
+
+    // This will highlight tiles the player can move to
+    // For now the movement is set at 1 per tile move
+    void OnMouseEnter()
+    {
+        if (Player.instance.playerCurrency >= 1)
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+
+    }
     // This will move the character to that tile on click
     // For now you can only move if you have more than 1 currency
     void OnMouseDown()
@@ -18,10 +34,16 @@ public class NormalTile : MonoBehaviour {
         }
     }
 
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white; 
+    }
+
     // Removes currency from player when they land on the tile, currently only takes away by 1
     void ArrivedOnTile()
     {
         Player.instance.playerCurrency--;
+
     }
 }
 
--- Unity files/Hare Tortoise Mechanics 2D/Assets/Scripts/Player.cs
+++ Unity files/Hare Tortoise Mechanics 2D/Assets/Scripts/Player.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
     public static Player instance;
     public float playerCurrency;
+    public Text currencyText;
+    public string currentTile;
+
 
     // Sets up player character
     void Awake()
     {
+
         instance = this;
         playerCurrency = 65;
+    }
+
+    // Shows how much currency player has on screen
+    void Update()
+    {
+        currencyText.text = "Currency: " + playerCurrency.ToString();
+    }
+
+    // Records what tile the player is on
+    // Trying to make waypointindex increase based on what tile player is on
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Tile")
+        {
+            currentTile = other.name;
+            Debug.Log("Tile is " + other.name);
+        }
     }
 }
