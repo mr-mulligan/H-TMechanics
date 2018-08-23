650d80d71dd0fb3fda40f3fc6c161c61a321ba0d
Aaron M <aaron95mulligan@gmail.com>
2018-08-23 05:06:56 +0100

    -Added start screen with button to choose players (code not finished yet) -Added ability to pause game -Made buttons at end of game replay or go back to menu

---
+++ Unity files/Hare Tortoise Mechanics 2D/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartScreen : MonoBehaviour {
+
+    //Any button on UI can use this to go to level
+    public void GoToGame()
+    {
+        SceneManager.LoadScene("Debug 2D Scene");
+    }
+    
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("Start Screen");
+    }
+}
